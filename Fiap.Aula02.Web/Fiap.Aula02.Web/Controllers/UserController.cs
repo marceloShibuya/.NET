beff4b59eb5acc102623daba91dd4583315e3dc2
marceloShibuya <50499486+marceloShibuya@users.noreply.github.com>
2020-08-18 11:58:25 -0300

    Model, View, Controller

---
+++ Fiap.Aula02.Web/Fiap.Aula02.Web/Controllers/UserController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Fiap.Aula02.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Fiap.Aula02.Web.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Cadastrar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Cadastrar(Usuario usuario)
+        {
+            ViewData["msg"] = "Sucesso";
+            TempData["msg"] = "Usuário registrado";
+            ViewBag.usu = usuario;
+            return View(usuario);
+        }
+
+    }
+}
