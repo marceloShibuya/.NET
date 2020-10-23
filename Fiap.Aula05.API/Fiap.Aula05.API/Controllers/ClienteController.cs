652897ca149c7a4f2cb96ecc2e3f23ff219028f2
marceloShibuya <50499486+marceloShibuya@users.noreply.github.com>
2020-10-23 09:00:34 -0300

    Finalização da API

--- Fiap.Aula05.API/Fiap.Aula05.API/Controllers/ClienteController.cs
+++ Fiap.Aula05.API/Fiap.Aula05.API/Controllers/ClienteController.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Fiap.Aula05.API.Migrations;
+using Fiap.Aula05.API.Models;
 using Fiap.Aula05.API.Repositories;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -20,17 +20,58 @@ namespace Fiap.Aula05.API.Controllers
             _clienteRepository = clienteRepository;
         }
 
+        [HttpGet("buscar")]
+        public IList<Cliente> Search(string nome)
+        {
+            return _clienteRepository.BuscarPor(c => c.Nome.Contains(nome));
+        }
+
         [HttpGet]
         public IList<Cliente> Get()
         {
             return _clienteRepository.Listar();
         }
 
-       
+        [HttpGet("{id}")]
+        public ActionResult<Cliente> Get(int id)
+        {
+            var cliente = _clienteRepository.Buscar(id);
+            if (cliente == null) return NotFound();
+            return cliente;
+        }
 
+        [HttpPost]
+        public ActionResult<Cliente> Post(Cliente cliente)
+        {
+            _clienteRepository.Cadastrar(cliente);
+            _clienteRepository.Salvar();
+            return CreatedAtAction("Get", new { id = cliente.ClienteId }, cliente);
+        }
 
+        [HttpPut("{id}")]
+        public ActionResult<Cliente> Put(int id, Cliente cliente)
+        {
+            var c = _clienteRepository.Buscar(id);
+            if (c == null) return NotFound();
 
+            cliente.ClienteId = id;
+            _clienteRepository.Atualizar(cliente);
+            _clienteRepository.Salvar();
+            
+            return Ok(cliente);
+        }
 
+        [HttpDelete("{id}")]
+        public ActionResult Delete(int id)
+        {
+            var c = _clienteRepository.Buscar(id);
+            if (c == null) return NotFound();
+
+            _clienteRepository.Remover(id);
+            _clienteRepository.Salvar();
+
+            return NoContent();
+        }
 
 
     }
